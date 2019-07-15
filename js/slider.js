93b842619c9c74d6c72f8a11e4ed2ba0b606ea9d
Ana Krmpotić <krmpoticana@gmail.com>
2019-07-15 20:04:12 +0200

    Maknute dodatne slike

--- js/slider.js
+++ js/slider.js
@@ -5,32 +5,38 @@ $(document).ready(function()
     var upperImages = $(".upperRow img");
     var bottomImages = $(".bottomRow img");
 
-    var cnt = 1;
+    var cntUpper = 1;
+    var cntBottom = 1;
     var sizeUpper = 0;
     var sizeBottom = 0;
     var translateUpper = upperImages[0].width + 10;
-    var translateBottom = 450;
+    var translateBottom = 330;
 
     sliderUpper.css("transform", "translateX(" + translateUpper + 'px)');
     sliderBottom.css("transform", "translateX(" + translateBottom + 'px)');
 
     $('.previous').on("click", function()
     {
-        if(cnt >= upperImages.length - 4)
+        if(cntUpper >= upperImages.length - 4 && cntBottom >= bottomImages.length - 4)
             return;
-        
-        sizeUpper = upperImages[cnt].width;
-        sizeBottom = bottomImages[cnt].width;
-        sliderUpper.css("transition", "transform 0.4s ease-in-out");
-        sliderBottom.css("transition", "transform 0.4s ease-in-out")
-        ++cnt;
 
-        translateUpper = translateUpper + sizeUpper + 10;
-        translateBottom = translateBottom + sizeBottom + 10;
-
-        sliderUpper.css("transform", "translateX(" + translateUpper + 'px)');
-        sliderBottom.css("transform", "translateX(" + translateBottom + 'px)');
-    })
+        if(cntUpper < upperImages.length - 4)
+        {
+            sizeUpper = upperImages[cntUpper].width;
+            sliderUpper.css("transition", "transform 0.4s ease-in-out");
+            ++cntUpper;
+            translateUpper = translateUpper + sizeUpper + 10;
+            sliderUpper.css("transform", "translateX(" + translateUpper + 'px)');
+        }
+        if(cntBottom < bottomImages.length - 4)
+        {
+            sizeBottom = bottomImages[cntBottom].width;
+            sliderBottom.css("transition", "transform 0.4s ease-in-out");
+            ++cntBottom;
+            translateBottom = translateBottom + sizeBottom + 10;
+            sliderBottom.css("transform", "translateX(" + translateBottom + 'px)');
+        }
+    });
 
     $('.previous').hover(function()
     {
@@ -43,20 +49,25 @@ $(document).ready(function()
 
     $('.next').on("click", function()
     {
-        if(cnt <= 0)
+        if(cntUpper <= 0 && cntBottom <= 0)
             return;
 
-        sizeUpper = upperImages[cnt - 1].width;
-        sizeBottom = bottomImages[cnt - 1].width;
-        sliderUpper.css("transition", "transform 0.4s ease-in-out");
-        sliderBottom.css("transition", "transform 0.4s ease-in-out");
-        --cnt;
-
-        translateUpper = translateUpper - sizeUpper - 10;
-        translateBottom = translateBottom - sizeBottom - 10;
-
-        sliderUpper.css("transform", "translateX(" + translateUpper + 'px)');
-        sliderBottom.css("transform", "translateX(" + translateBottom + 'px)');
+        if(cntUpper > 0)
+        {
+            sizeUpper = upperImages[cntUpper - 1].width;
+            sliderUpper.css("transition", "transform 0.4s ease-in-out");
+            --cntUpper;
+            translateUpper = translateUpper - sizeUpper - 10;
+            sliderUpper.css("transform", "translateX(" + translateUpper + 'px)');
+        }
+        if(cntBottom > 0)
+        {
+            sizeBottom = bottomImages[cntBottom - 1].width;
+            sliderBottom.css("transition", "transform 0.4s ease-in-out");
+            --cntBottom;
+            translateBottom = translateBottom - sizeBottom - 10;
+            sliderBottom.css("transform", "translateX(" + translateBottom + 'px)');
+        }
     })
 
     $('.next').hover(function()
@@ -70,37 +81,43 @@ $(document).ready(function()
 
     sliderUpper.on("transitionend", function()
     {      
-        if(upperImages[cnt].id === 'firstUpper')
+        if(upperImages[cntUpper].id === 'firstUpper')
         {
             sliderUpper.css("transition-duration", "0s");
-            sliderBottom.css("transition-duration", "0s");
-
-            cnt = 1;
-
+            cntUpper = 1;
             translateUpper = upperImages[0].width + 10;
-            translateBottom = 450;
-
             sliderUpper.css("transform", "translateX(" + translateUpper + 'px)');
+        }
+
+        if(bottomImages[cntBottom].id === 'firstBottom')
+        {
+            sliderBottom.css("transition-duration", "0s");
+            cntBottom = 1;
+            translateBottom = 330;
             sliderBottom.css("transform", "translateX(" + translateBottom + 'px)');
         }
 
-        if(upperImages[cnt].id === 'lastUpper')
+        if(upperImages[cntUpper].id === 'lastUpper')
         {
             sliderUpper.css("transition", "none");
-            sliderBottom.css("transition", "none");
-
-            cnt = upperImages.length - 5;
-
+            cntUpper = upperImages.length - 5;
             translateUpper = 0;
-            translateBottom = 0;
 
             for(var i = 1; i <= upperImages.length - 5; ++i)
                 translateUpper = translateUpper + upperImages[i].width + 10;
+
+            sliderUpper.css("transform", "translateX(" + translateUpper + 'px)');
+        }
+
+        if(bottomImages[cntBottom].id === 'lastBottom')
+        {
+            sliderBottom.css("transition", "none");
+            cntBottom = bottomImages.length - 5;
+            translateBottom = 0;
             for(var i = 1; i <= bottomImages.length - 5; ++i)
                 translateBottom = translateBottom + bottomImages[i].width + 10;
+            translateBottom = translateBottom - 30;
 
-            translateBottom = translateBottom - 40;
-            sliderUpper.css("transform", "translateX(" + translateUpper + 'px)');
             sliderBottom.css("transform", "translateX(" + translateBottom + 'px)');
         }
     });
